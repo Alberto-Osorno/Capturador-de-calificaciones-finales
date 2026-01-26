b81b496ea58c5eccb12a30bf67a35b8b7155b0e5
JoseLuisBasulto <joseluisbasulto22@gmail.com>
2026-01-26 09:44:37 -0600

    Se agregó S/C en el archivo pdf

--- src/main/java/basultosorno/ManipuladorPDF.java
+++ src/main/java/basultosorno/ManipuladorPDF.java
@@ -17,6 +17,9 @@ public class ManipuladorPDF {
             PdfDocument pdf = new PdfDocument(escribir);
             Document documento = new Document(pdf);
 
+            documento.add(new Paragraph("Reporte de Calificaciones Finales"));
+            documento.add(new Paragraph(" "));
+
             //Creación de la tabla
             float[] anchoDeLasColumnas = {200F, 200F, 200F}; //Arreglo de floats que simboliza cuantas columnas va a tener la tabla y de quá tamaño será cada columna
             Table tabla = new Table(anchoDeLasColumnas);
@@ -30,7 +33,12 @@ public class ManipuladorPDF {
             for(Estudiante estudiante : estudiantes){
                 tabla.addCell(new Cell().add(new Paragraph(estudiante.getMatricula())));
                 tabla.addCell(new Cell().add(new Paragraph("Diseño de software")));
-                tabla.addCell(new Cell().add(new Paragraph(String.valueOf(estudiante.getCalificacion()))));
+                if(estudiante.getCalificacion() == 0){
+                    tabla.addCell(new Cell().add(new Paragraph("S/C")));
+                }else{
+                    tabla.addCell(new Cell().add(new Paragraph(String.valueOf(estudiante.getCalificacion()))));
+
+                }
             }
 
             documento.add(tabla);
