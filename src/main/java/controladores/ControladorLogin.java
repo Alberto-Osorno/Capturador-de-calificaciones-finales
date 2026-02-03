0f5047620a29d79cf6ade19f4068e123268bd8ee
JoseLuisBasulto <joseluisbasulto22@gmail.com>
2026-02-02 19:53:31 -0600

    pequeñas modificaciones en controlador login

--- src/main/java/controladores/ControladorLogin.java
+++ src/main/java/controladores/ControladorLogin.java
@@ -5,7 +5,6 @@ import basultosorno.*;
 import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
-import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 
